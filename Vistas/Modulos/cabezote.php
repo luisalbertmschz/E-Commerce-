a4b1fa94cc0a342172ebf626db8f31a7651a710f
Albert <cerebrito07@hotmail.com>
2019-06-05 21:02:05 -0400

    Añadiendo colores  a las secciones superiores

--- Vistas/Modulos/cabezote.php
+++ Vistas/Modulos/cabezote.php
@@ -1,1 +1,311 @@
-<h1>Hola, esto del cabezote</h1>
+<!-- 
+//
+// ─── TOP  ───────────────────────────────────────────────────────────────────
+// 
+-->
+  
+
+   <div class="container-fluid  barraSuperior" id="top">
+       <div class="container">
+           <div class="row">
+           
+                <!--     
+                //
+                // ─── ENLANCES A REDES SOCIALES ───────────────────────────────────
+                //
+                -->
+              
+
+                <div class="col-lg-9 col-md-9 col-sm-8 col-xs-12 social">
+                
+                    <ul>
+
+                        <li>
+                            <a href="http://facebook.com/" target="_blank">
+                                <i class="fa fa-facebook redSocial facebookBlanco" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="http://youtube.com/" target="_blank">
+                                <i class="fa fa-youtube redSocial youtubeBlanco" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a href="http://twitter.com/" target="_blank">
+                                <i class="fa fa-twitter redSocial twitterBlanco" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                     <!--    <li>
+                            <a href="http://google.com/" target="_blank">
+                                <i class="fa fa-google-plus redSocial googleBlanco" aria-hidden="true"></i>
+                            </a>
+                        </li> -->
+
+                        <li>
+                            <a href="http://instagram.com/" target="_blank">
+                                <i class="fa fa-instagram redSocial instagramBlanco" aria-hidden="true"></i>
+                            </a>
+                        </li>
+
+                    </ul>
+                
+                </div>
+
+              <!--
+               //
+              // ─── SECCION DE REGISTRO DE LA CABEZERA SUPERIOR ─────────────────
+              //
+             -->
+
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 Registro">
+                
+                    <ul>
+                                
+                        <li><a href="#modalIngreso" data-toggle="modal">Identificate, inicia sesión</a></li>
+                        <li>|</li>
+                        <li><a href="#modalRegistro" data-toggle="modal">Nuevo cliente ? Cree una cuenta</a></li>
+                    
+                    </ul>
+                
+                
+                
+                </div>          
+           </div>
+       </div>
+   </div>
+
+<!-- 
+//
+// ─── HEADER O CABEZERA ──────────────────────────────────────────────────────────
+// 
+-->
+
+
+<header class="container-fluid">
+  <div class="container">
+     <div class="row" id="Cabezote">
+        	
+			<div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="logotipo">
+				
+				<a href="#">
+						
+					<img src="http://localhost/E-commerce-Backend/Vistas/plantilla/large_techtoot_0.png" class="img-responsive">
+
+				</a>
+                
+                 <!--  
+                 //
+                // LOGOS ALTERNATIVOS
+                //
+                -->
+                
+
+
+                <!--     
+                <a href="#">
+						
+					<img src="http://localhost/E-commerce-Backend/Vistas/plantilla/large_techstream.png" class="img-responsive">
+
+				</a>
+
+
+                <a href="#">
+						
+					<img src="http://localhost/E-commerce-Backend/Vistas/plantilla/large_TechTray_s.png" class="img-responsive">
+
+				</a>
+
+
+
+                <a href="#">
+						
+                        <img src="http://localhost/E-commerce-Backend/Vistas/plantilla/large_usabilitech_0.png" class="img-responsive">
+    
+                    </a>
+                -->
+
+             
+				
+			</div>
+
+            <!-- 
+            //
+            // ─── CATEGORIAS Y BUSCADOR DE LA TIENDA ──────────────────────────
+            //
+            -->
+
+            <div class="col-lg-6 col-md-6 col-sm-8 col-xs-12">
+
+                    <!-- 
+                    //
+                    // ─── BOTON DE ACCESO A LAS CATEGORIAS ─────────────────────────────
+                    //
+                    -->
+
+                    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 backColor" id="btnCategorias">
+                            
+                            <p>Categorías
+                            
+                                <span class="pull-right">
+                                    <i class="fa fa-bars" aria-hidden="true"></i>
+                                </span>
+                            
+                            </p>
+
+                    </div> 
+
+                    <!--    
+                    //
+                    //─── BOTON DE BUSQUEDA O SEARCH BAR ─────────────────────────────
+                    //
+                    -->
+
+                    <div class="input-group col-lg-8 col-md-8 col-sm-8 col-xs-12" id="buscador">
+					
+                        <input type="search" name="buscar" class="form-control" placeholder="Buscar...">	
+
+                        <span class="input-group-btn">
+                            
+                            <a href="#">
+
+                                <button class="btn btn-default backColor" type="submit">
+                                    
+                                    <i class="fa fa-search"></i>
+
+                                </button>
+
+                            </a>
+
+                        </span>
+
+				     </div>
+
+            </div> 
+
+            <!--             
+            //
+            // ─── SECCION DEL CARRITO DE COMPAS CON PRECIO Y CANTIDAD DE PRODUCTOS ──────── 
+            //
+            -->
+
+
+            <div class="col-lg-3 col-md-3 col-sm-2 col-xs-12" id="carrito">
+				
+				<a href="#">
+
+					<button class="btn btn-default pull-left backColor"> 
+						
+						<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+					
+					</button>
+				
+				</a>	
+
+				<p> Carrito <span class="cantidadCesta"> 3 </span> <br> USD $ <span class="sumaCesta"> 20 </span></p>	
+
+			</div>
+
+        </div>
+
+       <!--      
+       //
+        // ─── CATEGORIAS Y SUB CATEGORIAS DE BUSQUEDA ─────────────────────
+        // 
+       -->
+
+
+       <div class="col-xs-12 backColor" id="categorias">
+			
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+				
+				<h4>
+					<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+				</h4>
+				
+				<hr>
+
+				<ul>
+					
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+								
+				</ul>
+
+			</div>	
+
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+				
+				<h4>
+					<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+				</h4>
+				
+				<hr>
+
+				<ul>
+					
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+								
+				</ul>
+
+			</div>	
+
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+				
+				<h4>
+					<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+				</h4>
+				
+				<hr>
+
+				<ul>
+					
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+								
+				</ul>
+
+			</div>	
+
+			<div class="col-lg-2 col-md-3 col-sm-4 col-xs-12">
+				
+				<h4>
+					<a href="#" class="pixelCategorias">Lorem Ipsum</a>
+				</h4>
+				
+				<hr>
+
+				<ul>
+					
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+					<li><a href="#" class="pixelSubCategorias">Lorem Ipsum</a></li>
+								
+				</ul>
+
+			</div>	
+
+		</div>
+
+            
+
+
+
+
+
+    </div>
+</header>
--- Vistas/plantilla.php
+++ Vistas/plantilla.php
@@ -6,27 +6,46 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
         <title>Star-Tech :: Tienda de tecnología</title>
+        
+        
+
         <meta name="description" content="Tienda en linea de venta de dispositivos eléctrónicos en general ">
         <meta name="keywords" content="Computadoras de escritorio, Laptops, Piezas PC, Star, tech">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
 
-        //
+          <!--     
+         //
         // ─── HOJA DE ESTILO E ICONOS DE FONTAWESOME ──────────────────────
+       //
+       -->
+
+        <link rel="stylesheet" href="Vistas/css/plugins/bootstrap.min.css">
+        <link rel="stylesheet" href="Vistas/css/plugins/font-awesome.min.css">
+        <link rel="stylesheet" href="Vistas\css\plantilla.css">
+
+
+         <!--   
+         //
+        // ─── FUENTE TRAIDA DESDE GOOGLE FONT UBUNUT Y UBUNTU CONDENSED ───
         //
-        
-        <link rel="stylesheet" href="css/plugins/bootstrap.min.css">
-        <link rel="stylesheet" href="css/plugins/font-awesome.min.css">
+        -->
+            
+
+
+        <link href="https://fonts.googleapis.com/css?family=Ubuntu|Ubuntu+Condensed&display=swap" rel="stylesheet">
         
     
 
+         <!-- 
+         // 
+         // ─── SCRIPTS DE BOOTSTRAP Y JQUERY ───────────────────────────────
         //
-        // ─── SCRIPTS DE BOOTSTRAP Y JQUERY ───────────────────────────────
-        //
-
-        <script src="js/plugins/jquery.min.js"></script>
-        <script src="js/plugins/bootstrap.min.js"></script>
+       -->
+        <script src="Vistas/js/plugins/jquery.min.js"></script>
+        <script src="Vistas/js/plugins/bootstrap.min.js"></script>
     
 
             
@@ -34,14 +53,12 @@
             
     </head>
     <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
 
-
+        <!-- 
         //
         // ─── BARRA SUPERIOR DEL COMEINZO DE PAGINA ───────────────────────
         //
+        -->
 
         <?php
 
